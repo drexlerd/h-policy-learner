293e2a0fd362fd4b532e9f0f7f7d05dcc8f60142
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-17 09:15:51 +0200

    added delivery.py

---
+++ learning/learner/delivery.py
@@ -0,0 +1,62 @@
+from learner.src.util.misc import update_dict
+from learner.src.driver import BENCHMARK_DIR
+
+
+def experiments():
+    base = dict(
+    )
+
+    exps = dict()
+
+    strips_base = update_dict(
+        base,
+        domain_filename=BENCHMARK_DIR / "delivery" / "domain.pddl",
+    )
+
+    exps["sketch_debug"] = update_dict(
+        strips_base,
+        pipeline="sketch_pipeline",
+        instance_filenames=training_instances(),
+        debug_features=["n_count(c_not(c_equal(r_primitive(at_g,0,1),r_primitive(at,0,1))))",  # 5
+                        "b_empty(c_primitive(empty,0))",  # 2
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)), r_primitive(adjacent,0,1), c_primitive(at_g,1))",  # 7
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)), r_primitive(adjacent,0,1), c_and(c_all(r_inverse(r_primitive(at_g,0,1)),c_bot),c_some(r_inverse(r_primitive(at,0,1)),c_primitive(package,0))))",  # 15
+                        # "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)),r_primitive(adjacent,0,1),c_not(c_all(r_inverse(r_primitive(at,0,1)),c_equal(r_primitive(at,0,1),r_primitive(at_g,0,1)))))" alternative feature
+        ],
+    )
+
+    exps["sketch"] = update_dict(
+        strips_base,
+        pipeline="sketch_pipeline",
+        instance_filenames=training_instances(),
+        max_states_per_instance=2000,
+    )
+
+    exps["hierarchy"] = update_dict(
+        strips_base,
+        pipeline="hierarchy_pipeline",
+        instance_filenames=training_instances(),
+        max_states_per_instance=2000,
+    )
+
+    exps["hierarchy_debug"] = update_dict(
+        strips_base,
+        pipeline="hierarchy_pipeline",
+        instance_filenames=training_instances(),
+        debug_features=["n_count(r_and(r_primitive(at,0,1),r_primitive(at_g,0,1)))",
+                        "b_empty(c_primitive(empty,0))",
+                        "n_count(r_and(r_primitive(at,0,1),r_primitive(at_g,0,1)))",
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)),r_primitive(adjacent,0,1),c_some(r_inverse(r_primitive(at,0,1)),c_primitive(package,0)))",
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)),r_primitive(adjacent,0,1),c_some(r_inverse(r_primitive(at_g,0,1)),c_top))",
+                        "n_concept_distance(c_some(r_inverse(r_primitive(at,0,1)),c_primitive(truck,0)), r_primitive(adjacent,0,1), c_and(c_all(r_inverse(r_primitive(at_g,0,1)),c_bot),c_some(r_inverse(r_primitive(at,0,1)),c_primitive(package,0))))"],
+        max_states_per_instance=2000,
+    )
+    return exps
+
+
+
+def training_instances():
+    instance_filenames = []
+    for instance_name in [f"instance_{h}_{w}_{n}_{seed}.pddl" for h in range(1,4) for w in range(1,4) for n in range(1,3) for seed in range(0,1) ]:
+        instance_filenames.append(BENCHMARK_DIR / "delivery" / "instances" / instance_name)
+    return instance_filenames

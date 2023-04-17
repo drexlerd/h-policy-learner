c83bb821fe186e6d1e796cae756c42aeb668a360
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-17 14:58:37 +0200

    removed writing state spaces to file

--- learning/learner/src/instance_data/instance_data.py
+++ learning/learner/src/instance_data/instance_data.py
@@ -32,7 +32,7 @@ class InstanceData:
     def set_state_space(self, state_space: dlplan.StateSpace):
         """ Set state space and writes it to a file. """
         self.state_space = state_space
-        write_file(self.instance_information.workspace / f"{self.instance_information.name}.dot", state_space.to_dot(1))
+        # write_file(self.instance_information.workspace / f"{self.instance_information.name}.dot", state_space.to_dot(1))
 
     def set_goal_distances(self, goal_distances: Dict[int, int]):
         self.goal_distances =  goal_distances

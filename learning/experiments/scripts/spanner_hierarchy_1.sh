b9b3d57aa169bdddb43d2d937d19dfbf6f19893e
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-27 10:17:22 +0200

    .

--- learning/experiments/scripts/spanner_hierarchy_1.sh
+++ learning/experiments/scripts/spanner_hierarchy_1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH -J spanner_hierarchy_1
-#SBATCH -t 7-00:00:00
+#SBATCH -t 5-00:00:00
 #SBATCH -C thin --exclusive
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=dominik.drexler@liu.se

e56f1fc67e44ac4d213bc643bbf9361b7ddaff92
Dominik Drexler <dominik.drexler@gmx.net>
2023-04-30 14:30:25 +0200

    updated names of script files

--- README.md
+++ README.md
@@ -34,37 +34,37 @@ sudo ./build-planners.sh
 ```console
 cd learning/experiments/scripts
 
-./blocks_4_clear_hierarchy_0.sh
-./blocks_4_clear_hierarchy_1.sh
-./blocks_4_clear_hierarchy_2.sh
+./blocks_4_clear_0.sh
+./blocks_4_clear_1.sh
+./blocks_4_clear_2.sh
 
-./blocks_4_on_hierarchy_0.sh
-./blocks_4_on_hierarchy_1.sh
-./blocks_4_on_hierarchy_2.sh
+./blocks_4_on_0.sh
+./blocks_4_on_1.sh
+./blocks_4_on_2.sh
 
-./delivery_hierarchy_0.sh
-./delivery_hierarchy_1.sh
-./delivery_hierarchy_2.sh
+./delivery_0.sh
+./delivery_1.sh
+./delivery_2.sh
 
-./gripper_hierarchy_0.sh
-./gripper_hierarchy_1.sh
-./gripper_hierarchy_2.sh
+./gripper_0.sh
+./gripper_1.sh
+./gripper_2.sh
 
-./miconic_hierarchy_0.sh
-./miconic_hierarchy_1.sh
-./miconic_hierarchy_2.sh
+./miconic_0.sh
+./miconic_1.sh
+./miconic_2.sh
 
-./reward_hierarchy_0.sh
-./reward_hierarchy_1.sh
-./reward_hierarchy_2.sh
+./reward_0.sh
+./reward_1.sh
+./reward_2.sh
 
-./spanner_hierarchy_0.sh
-./spanner_hierarchy_1.sh
-./spanner_hierarchy_2.sh
+./spanner_0.sh
+./spanner_1.sh
+./spanner_2.sh
 
-./visitall_hierarchy_0.sh
-./visitall_hierarchy_1.sh
-./visitall_hierarchy_2.sh
+./visitall_0.sh
+./visitall_1.sh
+./visitall_2.sh
 ```
 
 ## Running the Testing Experiments
--- learning/experiments/scripts/submit.sh
+++ learning/experiments/scripts/submit.sh
@@ -2,56 +2,55 @@
 
 # e.g. dfsplan=${PARTITION}
 PARTITION=$1
-PIPELINE=$2
 
-# We timeout in blocks_3_general
-# sbatch -A ${PARTITION} blocks_3_general_${PIPELINE}_0.sh
-# sbatch -A ${PARTITION} blocks_3_general_${PIPELINE}_1.sh
-# sbatch -A ${PARTITION} blocks_3_general_${PIPELINE}_2.sh
+# We timeout in blocks_3
+# sbatch -A ${PARTITION} blocks_3_0.sh
+# sbatch -A ${PARTITION} blocks_3_1.sh
+# sbatch -A ${PARTITION} blocks_3_2.sh
 
-# We timout in blocks_4_general
-# sbatch -A ${PARTITION} blocks_4_general_${PIPELINE}_0.sh
-# sbatch -A ${PARTITION} blocks_4_general_${PIPELINE}_1.sh
-# sbatch -A ${PARTITION} blocks_4_general_${PIPELINE}_2.sh
+# We timout in blocks_4
+# sbatch -A ${PARTITION} blocks_4_0.sh
+# sbatch -A ${PARTITION} blocks_4_1.sh
+# sbatch -A ${PARTITION} blocks_4_2.sh
 
-sbatch -A ${PARTITION} blocks_4_clear_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} blocks_4_clear_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} blocks_4_clear_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} blocks_4_clear_0.sh
+sbatch -A ${PARTITION} blocks_4_clear_1.sh
+sbatch -A ${PARTITION} blocks_4_clear_2.sh
 
-sbatch -A ${PARTITION} blocks_4_on_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} blocks_4_on_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} blocks_4_on_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} blocks_4_on_0.sh
+sbatch -A ${PARTITION} blocks_4_on_1.sh
+sbatch -A ${PARTITION} blocks_4_on_2.sh
 
 # We timout in childsnack
-# sbatch -A ${PARTITION} childsnack_${PIPELINE}_0.sh  does not exist over C2 features
-# sbatch -A ${PARTITION} childsnack_${PIPELINE}_1.sh
-# sbatch -A ${PARTITION} childsnack_${PIPELINE}_2.sh
+# sbatch -A ${PARTITION} childsnack_0.sh  does not exist over C2 features
+# sbatch -A ${PARTITION} childsnack_1.sh
+# sbatch -A ${PARTITION} childsnack_2.sh
 
-sbatch -A ${PARTITION} delivery_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} delivery_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} delivery_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} delivery_0.sh
+sbatch -A ${PARTITION} delivery_1.sh
+sbatch -A ${PARTITION} delivery_2.sh
 
 # We timeout on grid
-# sbatch -A ${PARTITION} grid_${PIPELINE}_0.sh
-# sbatch -A ${PARTITION} grid_${PIPELINE}_1.sh
-# sbatch -A ${PARTITION} grid_${PIPELINE}_2.sh
+# sbatch -A ${PARTITION} grid_0.sh
+# sbatch -A ${PARTITION} grid_1.sh
+# sbatch -A ${PARTITION} grid_2.sh
 
-sbatch -A ${PARTITION} gripper_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} gripper_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} gripper_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} gripper_0.sh
+sbatch -A ${PARTITION} gripper_1.sh
+sbatch -A ${PARTITION} gripper_2.sh
 
-sbatch -A ${PARTITION} miconic_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} miconic_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} miconic_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} miconic_0.sh
+sbatch -A ${PARTITION} miconic_1.sh
+sbatch -A ${PARTITION} miconic_2.sh
 
-sbatch -A ${PARTITION} reward_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} reward_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} reward_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} reward_0.sh
+sbatch -A ${PARTITION} reward_1.sh
+sbatch -A ${PARTITION} reward_2.sh
 
-sbatch -A ${PARTITION} spanner_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} spanner_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} spanner_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} spanner_0.sh
+sbatch -A ${PARTITION} spanner_1.sh
+sbatch -A ${PARTITION} spanner_2.sh
 
-sbatch -A ${PARTITION} visitall_${PIPELINE}_0.sh
-sbatch -A ${PARTITION} visitall_${PIPELINE}_1.sh
-sbatch -A ${PARTITION} visitall_${PIPELINE}_2.sh
+sbatch -A ${PARTITION} visitall_0.sh
+sbatch -A ${PARTITION} visitall_1.sh
+sbatch -A ${PARTITION} visitall_2.sh
